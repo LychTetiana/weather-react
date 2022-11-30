d0360d69cdb6517030a771fafaef720c022d7e9a
LychTetiana <tetiana_tomashchuk@ukr.net>
2022-11-30 23:40:30 +0200

    Add react-loader-spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-
+import { Dna } from 'react-loader-spinner';
 
 export default function Weather(props) {
 function handleResponse(response) {
@@ -10,5 +10,14 @@ function handleResponse(response) {
     let apiKey = "8a104eff40d67002b71f619e6f4833ec";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&appid=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-return <h2>Hello from Weather</h2>
+return (
+    <Dna
+    visible={true}
+    height="80"
+    width="80"
+    ariaLabel="dna-loading"
+    wrapperStyle={{}}
+    wrapperClass="dna-wrapper"
+  />
+)
   }
